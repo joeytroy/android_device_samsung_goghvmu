cbd8a95ed3863566bb19ea4e7abeaf5ba09f5360
Joey Troy <joey@joeytroy.com>
2013-05-13 22:53:12 -0600

    Device Update
    Added missing files in config folder. Created README with detailed instructions on building jellybean. Updated device_goghvmu.mk as some files
    where not need for goghvmu. Updated blobs file bases on files needed for goghvmu to build properly. Updated setup-makefiles.sh to include obj
    lib for device files. Working behind xInterlopeRx has been a pleasure as he really has taken the time to write proper device files! Something
    to be learned and remembered for future development!

--- device_goghvmu.mk
+++ device_goghvmu.mk
@@ -180,12 +180,7 @@ PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
     
 # NFCEE access control
-ifeq ($(TARGET_BUILD_VARIANT),user)
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/config/nfcee_access.xml
-else
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/config/nfcee_access_debug.xml
-endif
-PRODUCT_COPY_FILES += $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/config/nfcee_access.xml:system/etc/nfcee_access.xml
 
 #audio
 #PRODUCT_COPY_FILES += \
